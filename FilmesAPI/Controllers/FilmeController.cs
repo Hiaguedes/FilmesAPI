a52061f29cf9bb827ea437b3274297aff13ff582
Hiaguedes <hiago@contasimples.com>
2022-01-06 23:48:42 -0300

    o que foi feito ate antes das ferias

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -8,13 +8,35 @@ namespace FilmesAPI.Controllers;
 public class FilmeController
 {
     private static List<Filme> filmes = new();
+
+    private static int ControllerId = 0;
     
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = ControllerId++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo); 
     }
-    
+
+    [HttpGet]
+    public IEnumerable<Filme> ListaFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult BuscaFilmePorId(int id)
+    {
+        Filme filmeBuscado = filmes.FirstOrDefault(film => film.Id == id);
+
+        if (filmeBuscado != null)
+        {
+            return Ok(filmeBuscado);
+        }
+
+        return NotFound();
+
+    }
     
 }
